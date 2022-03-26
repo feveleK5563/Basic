9d776171da709c0be571b2e55c6cd9f6230758ba
feveleK5563 <buncha102828@gmail.com>
2022-03-27 00:46:41 +0900

    デバッグビルドとリリースビルドができるようにした

--- build/build.bat
+++ build/build.bat
@@ -3,6 +3,9 @@
 rem プロジェクト名定義
 set PROJECT_NAME=basic
 
+rem ビルドオプション
+set IS_DEBUG_BUILD=%1
+
 rem 作業ディレクトリ
 cd /d %~dp0
 set MAKE_DIR=%~dp0\make
@@ -17,16 +20,16 @@ if not exist %MAKE_DIR% (
 cd %MAKE_DIR%
 
 rem cmakeでなんかファイルいっぱい作る
-cmake .. -G "MinGW Makefiles" -D CMAKE_PROJECT_NAME="%PROJECT_NAME%"
+cmake .. -G "MinGW Makefiles" ^
+         -D CMAKE_PROJECT_NAME="%PROJECT_NAME%" ^
+         -D IS_DEBUG_BUILD=%IS_DEBUG_BUILD%
 if %errorlevel% neq 0 (
-    pause
     exit /b 1
 )
 
 rem ビルド
 cmake --build .
 if %errorlevel% neq 0 (
-    pause
     exit /b 1
 )
 
@@ -34,7 +37,6 @@ rem 実行ファイルを移動させる
 echo %EXE_FILE%
 if not exist %EXE_FILE% (
     echo 実行ファイルがありません
-    pause
     exit /b 1
 )
 
